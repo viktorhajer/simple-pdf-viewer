7ea803616af1f9f5fa9f41d8efb3127f9a3d0551
viktorhajer <hajer.viktor@gmail.com>
2018-02-18 13:29:45 +0100

    Fix npm package generation

--- libs/simple-pdf-viewer/index.ts
+++
@@ -1,3 +0,0 @@
-export { SimplePdfViewerModule } from './src/simplePdfViewer.module';
-export { SimplePdfViewerComponent } from './src/simplePdfViewer.component';
-export { SimpleSearchState, SimpleDocumentInfo, SimpleOutlineNode, SimpleProgressData, SimplePDFBookmark } from './src/simplePdfViewer.models';
---
+++ public_api.ts
@@ -0,0 +1,3 @@
+export { SimplePdfViewerModule } from './libs/simple-pdf-viewer/src/simplePdfViewer.module';
+export { SimplePdfViewerComponent } from './libs/simple-pdf-viewer/src/simplePdfViewer.component';
+export { SimpleSearchState, SimpleDocumentInfo, SimpleOutlineNode, SimpleProgressData, SimplePDFBookmark } from './libs/simple-pdf-viewer/src/simplePdfViewer.models';
