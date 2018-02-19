9135948dbe60692dd28587f3d0406fa15ec18843
Viktor Hajer <hajer.viktor@gmail.com>
2018-02-19 15:04:22 +0100

    Update public_api.ts

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,3 @@
 export { SimplePdfViewerModule } from './libs/simple-pdf-viewer/src/simplePdfViewer.module';
 export { SimplePdfViewerComponent } from './libs/simple-pdf-viewer/src/simplePdfViewer.component';
-export { SimpleSearchState, SimpleDocumentInfo, SimpleOutlineNode, SimpleProgressData, SimplePDFBookmark } from './libs/simple-pdf-viewer/src/simplePdfViewer.models';
+export { SimpleSearchState, SimpleDocumentInfo, SimpleOutlineNode, SimpleProgressData, SimpleSearchOptions, SimplePDFBookmark } from './libs/simple-pdf-viewer/src/simplePdfViewer.models';
